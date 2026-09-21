143d1931d999b0b6d95a30c2e33c93b881129e93
Polina <polinakvyat9@gmail.com>
2026-09-21 23:41:13 +0300

    Fix

--- app/(private routes)/notes/profile/page.tsx
+++ app/(private routes)/notes/profile/page.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next';
-import { getMe } from '@/lib/api/clientApi';
+import { getMe } from '@/lib/api/serverApi';
 
 export const metadata: Metadata = {
-  title: 'Profile | Application',
-};
+    title: 'Profile | Application',
+}
 
 export default async function ProfilePage() {
   const user = await getMe();
@@ -12,7 +12,6 @@ export default async function ProfilePage() {
     <main>
       <h1>Profile</h1>
       <p>Username: {user?.username || '—'}</p>
-      <p>Email: {user?.email || '—'}</p>
     </main>
   );
 }
